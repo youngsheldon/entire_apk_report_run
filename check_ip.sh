e7007aa7dfb0680137cc2f26eb9ca718b6fb1e56
youngsheldon <1349739532@qq.com>
2016-12-07 09:26:29 +0800

    v10

--- check_ip.sh
+++ check_ip.sh
@@ -2,6 +2,7 @@
 # @Author: anchen
 # @Date:   2016-12-06 19:04:37
 # @Last Modified by:   anchen
-# @Last Modified time: 2016-12-07 09:17:09
+# @Last Modified time: 2016-12-07 09:25:47
+export LD_LIBRARY_PATH="/opt/smmc/apk_parse/entire_apk_report_run/check/lib"
 cd /opt/smmc/apk_parse/entire_apk_report_run/check/
 ipcheck $1 $2
