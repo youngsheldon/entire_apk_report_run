118f59f3fc32920e0cee674a9a6985cb1bf25e08
youngsheldon <1349739532@qq.com>
2016-12-07 09:19:44 +0800

    v9

--- check_ip.sh
+++ check_ip.sh
@@ -2,6 +2,6 @@
 # @Author: anchen
 # @Date:   2016-12-06 19:04:37
 # @Last Modified by:   anchen
-# @Last Modified time: 2016-12-06 20:50:50
+# @Last Modified time: 2016-12-07 09:17:09
 cd /opt/smmc/apk_parse/entire_apk_report_run/check/
-ipcheck 1 $1
+ipcheck $1 $2
